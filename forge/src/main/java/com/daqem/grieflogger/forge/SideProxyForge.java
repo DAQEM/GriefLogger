9e3fbf6c9b626b0fa90ea7ab9fa20e4beb75444a
DAQEM <daqemyt@gmail.com>
2024-02-06 00:39:35 +0100

    Fixed error: package net.minecraft.core.registries does not exist

--- forge/src/main/java/com/daqem/grieflogger/forge/SideProxyForge.java
+++ forge/src/main/java/com/daqem/grieflogger/forge/SideProxyForge.java
@@ -1,13 +1,7 @@
 package com.daqem.grieflogger.forge;
 
 import com.daqem.grieflogger.GriefLogger;
-import com.daqem.grieflogger.command.argument.FilterArgument;
 import dev.architectury.platform.forge.EventBuses;
-import dev.architectury.registry.registries.DeferredRegister;
-import net.minecraft.commands.synchronization.ArgumentTypeInfo;
-import net.minecraft.commands.synchronization.ArgumentTypeInfos;
-import net.minecraft.commands.synchronization.SingletonArgumentInfo;
-import net.minecraft.core.registries.Registries;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class SideProxyForge {
