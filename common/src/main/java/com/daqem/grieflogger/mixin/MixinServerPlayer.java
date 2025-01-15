8803db15cb12b24d42364df06c45c8505631327c
steve6472 <20379161+steve6472@users.noreply.github.com>
2025-01-15 11:53:27 +0100

    Fixed item drop logging

--- common/src/main/java/com/daqem/grieflogger/mixin/MixinServerPlayer.java
+++ common/src/main/java/com/daqem/grieflogger/mixin/MixinServerPlayer.java
@@ -16,7 +16,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -110,6 +112,13 @@ public abstract class MixinServerPlayer extends Player implements GriefLoggerSer
         if (!grieflogger$itemQueue.isEmpty()) {
             Services.ITEM.insertMap(getUUID(), level(), blockPosition(), new HashMap<>(grieflogger$itemQueue));
             grieflogger$itemQueue.clear();
+        }
+    }
+
+    @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("RETURN"))
+    private void drop(ItemStack itemStack, boolean bl, boolean bl2, CallbackInfoReturnable<ItemEntity> cir) {
+        if (!itemStack.isEmpty()) {
+            this.griefLogger$addItemToQueue(ItemAction.DROP_ITEM, new SimpleItemStack(itemStack));
         }
     }
 
