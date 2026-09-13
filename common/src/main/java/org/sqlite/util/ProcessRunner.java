7636b3760ab1f3797f6412e4664807622486d850
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-09-13 12:49:21 +0200

    feat: add Docker support with multiple OS configurations and server setup scripts

--- common/src/main/java/org/sqlite/util/ProcessRunner.java
+++ common/src/main/java/org/sqlite/util/ProcessRunner.java
@@ -1,4 +1,19 @@
 package org.sqlite.util;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class ProcessRunner {
+
+    public ProcessRunner() {
+    }
+
+    String runAndWaitFor(String command) throws IOException, InterruptedException {
+        return "";
+    }
+
+    String runAndWaitFor(String command, long timeout, TimeUnit unit)
+            throws IOException, InterruptedException {
+        return "";
+    }
 }
